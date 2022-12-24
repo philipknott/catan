cfff296be558bc12acd55fb37a335e29384c7103
Philip Knott <filupnot@gmail.com>
2022-12-23 18:54:59 -0700

    fix: changed axial coordinates to whole numbers

--- src/lib/util/helpers.ts
+++ src/lib/util/helpers.ts
@@ -1,15 +1,20 @@
 import type { AxialCoords, SquareCoords } from './types';
 
-export const convertAxialToSquare = (axialCoord: AxialCoords): SquareCoords => {
-	const [q, r, s] = [axialCoord.q, axialCoord.r, -(axialCoord.q + axialCoord.r)];
+export const convertAxialToSquare = (ac: AxialCoords): SquareCoords => {
+	const q = ac.q / 6;
+	const r = ac.r / 6;
+	const s = -q - r;
+
 	return {
 		x: 12.5 * (q - r / 2 - s / 2) + 50,
 		y: 10 * (r - s) + 50,
 	};
 };
 
-export const calculateEdgeRotation = (axialCoord: AxialCoords): number => {
-	const { q, r } = axialCoord;
+export const calculateEdgeRotation = (ac: AxialCoords): number => {
+	const q = ac.q / 6;
+	const r = ac.r / 6;
+
 	const [wq, wr] = [q % 1 == 0, r % 1 == 0];
 
 	if (!wq && wr) {
